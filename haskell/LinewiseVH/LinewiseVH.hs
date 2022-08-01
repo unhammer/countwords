62f86f6f58f2093941b8f6000beb100473b27199
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2022-08-01 21:53:18 +0200

    Add linewise vector-hashtables Haskell version from jaror

---
+++ haskell/LinewiseVH/LinewiseVH.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Data.Foldable
+import           Data.List       (sortBy)
+import           Data.Ord        (Down (..), comparing)
+import qualified Data.Text       as T
+import qualified Data.Text.IO    as T
+
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Unboxed.Mutable as UM
+import qualified Data.Vector.Hashtables as H
+
+import Control.Monad
+import Control.Monad.Primitive
+import System.IO
+
+type HashTable k v = H.Dictionary (PrimState IO) VM.MVector k UM.MVector v
+
+unlessM :: Monad m => m Bool -> m () -> m ()
+unlessM m1 m2 = m1 >>= \b -> unless b m2
+
+main :: IO ()
+main = do
+  t <- H.initialize 10000 :: IO (HashTable T.Text Int)
+  let
+    go = unlessM isEOF $ do
+      words <- T.words . T.toLower <$> T.getLine
+      traverse_ (H.alter t (Just . maybe 1 (+ 1))) words
+      go
+  go
+  xs <- sortBy (comparing (Down . snd)) <$> H.toList t
+  traverse_ (\(w, i) -> T.putStrLn (w `T.append` " " `T.append` T.pack (show i))) xs
