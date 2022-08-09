de7b8a1996fb270042a9b0b82023011f8fab0644
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2022-08-09 15:06:22 +0200

    Warn on differing outputs before benchmarking

--- haskell/run.sh
+++ haskell/run.sh
@@ -2,9 +2,19 @@
 
 stack build || exit 1
 
+normalize () {
+    # tac on a sponge at the end to avoid BrokenPipeError from diff's short-circuiting
+    grep -v '^ ' | python3 ../normalize.py | tac | tac
+}
+
 stack --bash-completion-index 2 --bash-completion-word stack --bash-completion-word build --bash-completion-word countwordsHS:exe: \
     | while read -r cexe; do
     exe=${cexe##countwordsHS:exe:}
+
+    if ! diff -q ../output.txt <(stack exec "$exe" <../kjvbible_x10.txt | normalize) &>/dev/null; then
+        echo "[1mWARNING: $exe produced unexpected output[0m" >&2
+    fi
+
     if command -V nix &>/dev/null; then
       # echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
       stack exec nix -- run 'nixpkgs#hyperfine' -- --warmup 1 "\"$exe\" <../kjvbible_x10.txt"
@@ -14,4 +24,5 @@ stack --bash-completion-index 2 --bash-completion-word stack --bash-completion-w
       echo "$exe:"
       stack exec /usr/bin/env -- time "$exe" <../kjvbible_x10.txt >/dev/null
     fi
+
 done
